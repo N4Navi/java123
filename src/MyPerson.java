0c9a04af2703bc68987c91e50f3676376fb3cc75
Java123 <navishka.2428cseaiml@kiet.edu>
2025-08-22 12:39:32 +0530

    202401100400123

---
+++ src/MyPerson.java
@@ -0,0 +1,9 @@
+public class MyPerson {
+    public static void main(String[] args) {
+        person p =new person("Ghaziabad");
+        p.getAddress();
+        SchoolStudent st = new SchoolStudent("Ghaziabad","B.tech");
+        st.getAddress();
+
+    }
+}
---
+++ src/SchoolStudent.java
@@ -0,0 +1,12 @@
+public class SchoolStudent extends person {
+    public String course;
+
+    public SchoolStudent(String address, String course) {
+        super(address);
+        this.course = course;
+    }
+    public void getCourse()
+    {
+        System.out.println("Course "+course);}
+
+}
