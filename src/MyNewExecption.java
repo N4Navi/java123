abafb78dfe874ce262ccb8b4df09344b0161cf3e
Lenovo <navishka.2428cseaiml@kiet.edu>
2025-08-29 11:26:54 +0530

    29 -08-2025  User Defined Exception

---
+++ src/MyNewExecption.java
@@ -0,0 +1,10 @@
+public class MyNewExecption {
+    public static void main(String[] args) {
+        UserDefinedException udf = new UserDefinedException();
+        try{
+        udf.validate(12);
+    } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
