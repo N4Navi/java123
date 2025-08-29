7723129355079c09576f6b2bd539531aef70c268
Lenovo <navishka.2428cseaiml@kiet.edu>
2025-08-29 11:49:46 +0530

    29 -08-2025  User Defined Exception

---
+++ src/Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+    private int balance =10000;
+    public void withdraw(int amount) {
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Cannot withdraw amount greater than Balance");
+        }
+        else{
+            balance -= amount;
+            System.out.println("Withdraw Successful,Remaining Balance: " + balance);
+        }
+    }
+}
---
+++ src/MyBank.java
@@ -0,0 +1,10 @@
+public class MyBank {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        try{
+        b.withdraw(1200);
+    } catch(InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
