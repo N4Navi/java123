7f98481ef5df469cadb8a6b00ff2264f45cc7bca
Lenovo <navishka.2428cseaiml@kiet.edu>
2025-09-02 11:39:27 +0530

    02-09-2025

---
+++ src/unit3/Array1.java
@@ -0,0 +1,17 @@
+package unit3;
+import java.util.Scanner;
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int a[] = new int[sc.nextInt()];
+        System.out.println("Enter Elements");
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
